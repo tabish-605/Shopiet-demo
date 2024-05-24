92ace228cdf7d80eddf14f7f8b73ea9cc222c35f
Austin <austinematurure@gmail.com>
2024-05-24 20:59:13 +0300

    Fixed navbar issue

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar.jsx';
 import App from './App.jsx';
 import './css/index.css';
