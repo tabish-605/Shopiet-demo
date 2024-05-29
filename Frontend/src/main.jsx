31761a8f3df67641a241413b68854957abbf0c6b
Austin <austinematurure@gmail.com>
2024-05-29 19:40:36 +0300

    Slow load fix test

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -16,19 +16,10 @@ import SearchPage from './SearchPage.jsx';
 import ProfileDetail from './ProfileDetail.jsx';
 import SavedItems from './SavedPage.jsx';
 import ErrorBoundary from './ErrorBoundary.jsx';
-import pnf from './assets/pnf-404.svg'
+import NotFound from './NotFound.jsx';
 
-const NotFound = () => {
-  return (
-    <div>
-      <div className='error-div flex-col'> <div className="eimg-cnt">
-        <img loading="lazy"  src={pnf} id='error-404 pnf' className="item-detail-image" />
-        </div></div>
-      <h2>Page Not Found</h2>
-      <p>Sorry, the page you are looking for does not exist.</p>
-    </div>
-  );
-};
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
