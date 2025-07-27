8049caf5aa731b40da1ecc1a868dec6b7d1f5aaa
tabish-605 <168262315+tabish-605@users.noreply.github.com>
2025-07-27 13:53:02 +0000

    working setup

--- Backend(Docked)/backend/settings.py
+++ Backend(Docked)/backend/settings.py
@@ -3,9 +3,15 @@
 
 import os
 from dotenv import load_dotenv
-
+from pathlib import Path
 # Load environment variables
+from django.conf import settings
+
+
 load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ROOT_URLCONF = 'backend.urls'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key-for-development-only')
@@ -78,3 +84,40 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'shopiet',
+    # add your app modules here, e.g. 'shopapp',
+]
+ 
+MIDDLEWARE = [
+'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
