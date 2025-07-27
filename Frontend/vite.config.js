ed9e2679e51266b84453d2ab551a1589d99840f0
tabish-605 <168262315+tabish-605@users.noreply.github.com>
2025-07-27 15:28:47 +0530

    Add files via upload

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      port: 3000,
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
 })
